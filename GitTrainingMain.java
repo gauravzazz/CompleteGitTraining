21edd40f88088f42f26ecbfbaf0183968412475d
gauravzazz <dddgaurav@gmail.com>
2022-01-04 00:16:11 +0530

    this is release feture on feture branch

--- GitTrainingMain.java
+++ GitTrainingMain.java
@@ -3,4 +3,9 @@ class Main{
   public static void main(String[] args){
     System.out.println("This is main feature");
   }
+
+  public static void releasefeature(){
+    System.out.println("This is feature release");
+  }
+
 }
